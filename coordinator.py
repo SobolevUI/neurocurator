b0cffb8bb26daeb9bfe75d67dd30e6e3cab36035
SobolevUI <69240596+SobolevUI@users.noreply.github.com>
2021-09-30 18:44:50 +0300

    Add files via upload

---
+++ coordinator.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt                                                 #Подключаем Matplotlib
+import re
+import pickle
+import pymorphy2
+import json
+from flask import Flask, jsonify, request
+import string
+# model=load_model('modell_curator.h5')
+log_file_coordinator='logs_coordinator.txt'
+with open('./key_words_coordinator.json', 'r') as fp:
+    key_words = json.load(fp)
+
+def converter(sentence):
+    list = []
+    words = sentence.split()
+    for item in words:
+        list.append(pymorphy2.MorphAnalyzer().parse(item)[0].normal_form)
+    return ' '.join(list)
+
+
+def clear_text(new_text):
+
+  pattern_number=r'[0-9]'
+  new_text=re.sub(pattern_number,'',new_text)
+  new_text=' '.join( [w for w in new_text.split() if len(w)>1] )
+  new_text=re.compile("[" + re.escape(string.punctuation) + "]").sub("", new_text)
+  return new_text
+
+
+
+def predict_tags(a):
+  
+  a=clear_text(a)
+  a=converter(a)
+  print(a)
+  keys=[ key_words[i] for i in key_words.keys() if i in a.lower()]
+  return set(keys)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def main():
+    with open (log_file_coordinator, mode='a',encoding='utf8') as f:
+        try:
+            # print(request)
+            data = request.form.get('key')
+            result=list(predict_tags(data))
+            res={}
+            for i,tag in enumerate(result):
+                res.update({i:tag})
+            return json.dumps(res,ensure_ascii=False).encode('utf8')
+        except Exception as e:
+            print(type(e),e,file=f)
+if __name__ == '__main__':
+    app.run(debug=False, port=2248)
